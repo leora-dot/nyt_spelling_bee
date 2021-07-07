ab96863633c6dc7454cc4bd543747559d10d2bf8
leora-dot <7536446+leora-dot@users.noreply.github.com>
2021-07-06 23:07:56 -0400

    add remove duplicates

--- data_generation/helper_functions.py
+++ data_generation/helper_functions.py
@@ -12,7 +12,13 @@ def file_to_word_list(input_file, min_letters = 0):
             if len(stripped_line) >= min_letters:
                 word_list.append(stripped_line)
 
+        word_list = remove_duplicates(word_list)
+
     return word_list
+
+def remove_duplicates(input_list):
+    unique_arr = np.unique(np.array(input_list))
+    return list(unique_arr)
 
 def filter_list(main_list, remove_list):
     return list(np.setdiff1d(main_list, remove_list))
