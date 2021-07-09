4ab89a920a1adc4b9cf009e0ce724b36df7a0325
leora-dot <7536446+leora-dot@users.noreply.github.com>
2021-07-09 12:35:24 -0400

    add inheritance for data generator

--- data_generation/data_generator.py
+++ data_generation/data_generator.py
@@ -17,22 +17,19 @@ from helper_functions import alphabet_string
 
 class DataGenerator():
 
-    def __init__(self, output_file, profanity_input_file, solver):
+    def __init__(self, output_file):
 
-        self.solver = solver
         self.output_file = output_file
-        self.profanity_input_file = profanity_input_file
-        self.data_unlogged = queue.Queue()
 
-        self.is_data_generation_active = True
-        self.is_data_logging_active = True
+        self.to_generate = queue.Queue() #Task IDs
+        self.to_process = queue.Queue() #Unprocessed Data
+        self.to_log = queue.Queue() #Unlogged data
+
+        self.is_generation_active = True
+        self.is_processing_active = True
+        self.is_logging_active = True
         self.is_active = True
 
-        self.combination_index = None
-        self.combination = None
-        self.combination_string = None
-
-        self.num_combinations_total = 657800 #26 choose 7
         self.progress_interval = 1 #time between progress loops in minutes
         self.block_time = 5 #time for blocking in seconds
 
@@ -40,43 +37,44 @@ class DataGenerator():
 
         print("Checking existing data log...")
 
-        logged_keys = pd.read_csv(self.output_file, usecols=["COMBINATION_INDEX"], squeeze = True)
+        logged_keys = pd.read_csv(self.output_file, usecols=[self.output_column_index], squeeze = True)
         logged_max = logged_keys.max()
 
         if np.isnan(logged_max):
             logged_max = 0
         self.last_logged_index = logged_max
 
-        self.print_progress()
+    def generate_tasks(self):
 
-    def generate_iterators(self):
+        for task_index in range(self.last_logged_index, self.last_index):
+            self.to_generate.put(task_index)
 
-        combinations = itertools.combinations(alphabet_string, 7)
-        combination_keys = itertools.count()
-        combinations_and_keys = zip(combination_keys, combinations)
+    def generate_data_loop(self):
 
-        start_index = self.last_logged_index
-        self.combinations_outstanding = itertools.islice(combinations_and_keys, start_index, self.num_combinations_total + 1)
+        while not self.to_generate.empty():
+            task_index = self.to_generate.get()
+            self.generate_data(task_index)
+            self.to_generate.task_done()
 
-    def generate_combination_data(self):
+        self.is_generation_active = False
 
-        for center_letter in self.combination:
-            output_dict = {}
-            validator = Validator(self.solver, self.combination, center_letter, self.profanity_input_file)
+    def process_data_loop(self):
 
-            output_dict["COMBINATION_INDEX"] = self.combination_index
-            output_dict["SEVEN_LETTERS"] = self.combination_string
-            output_dict["CENTER_LETTER"] = center_letter
-            output_dict["NUM_SOLUTIONS"] = validator.num_solutions()
-            output_dict["IS_PROFANE"] = validator.is_profane()
-            output_dict["IS_PANGRAM"] = validator.is_pangram()
-
-            self.data_unlogged.put(output_dict)
+        while self.is_processing_active:
+            try:
+                data = self.to_process.get(block = False)
+                self.process_data(data)
+                self.to_process.task_done()
+            except queue.Empty:
+                if self.is_generation_active:
+                    time.sleep(self.block_time)
+                else:
+                    self.is_processing_active = False
 
     def generate_file_objects(self):
 
         self.file = open(self.output_file, 'a', newline='')
-        self.dictwriter_object = DictWriter(self.file, fieldnames= ["COMBINATION_INDEX", "SEVEN_LETTERS", "CENTER_LETTER", "NUM_SOLUTIONS", "IS_PROFANE", "IS_PANGRAM"])
+        self.dictwriter_object = DictWriter(self.file, fieldnames= self.output_columns_list)
 
     def log_data_loop(self):
 
@@ -85,13 +83,13 @@ class DataGenerator():
 
         self.generate_file_objects()
 
-        while self.is_data_logging_active:
+        while self.is_logging_active:
 
             try:
-                data = self.data_unlogged.get(block = False)
-                self.dictwriter_object.writerow(data)
-                self.last_logged_index = data.get("COMBINATION_INDEX")
-                self.data_unlogged.task_done()
+                output_dict = self.to_log.get(block = False)
+                self.dictwriter_object.writerow(output_dict)
+                self.last_logged_index = output_dict.get(self.output_column_index)
+                self.to_log.task_done()
                 write_counter +=1
 
                 if write_counter == counter_max:
@@ -101,19 +99,19 @@ class DataGenerator():
 
             except queue.Empty:
 
-                if self.is_data_generation_active:
+                if self.is_generation_active:
                     time.sleep(self.block_time)
 
                 else:
                     self.file.close()
-                    self.is_data_logging_active = False
+                    self.is_logging_active = False
                     self.print_progress()
 
     def print_progress(self):
 
         now = datetime.now()
         now_string = now.strftime("%m/%d/%Y %H:%M:%S")
-        print("{:,} / {:,} combinations logged ({:.2%} complete) at {}".format(self.last_logged_index, self.num_combinations_total, self.last_logged_index/self.num_combinations_total, now_string))
+        print("{:,} / {:,} combinations logged ({:.2%} complete) at {}".format(self.last_logged_index, self.last_index, self.last_logged_index/self.last_index, now_string))
 
     def print_progress_loop(self):
 
@@ -123,39 +121,66 @@ class DataGenerator():
             self.print_progress()
             time.sleep(sleep_time)
 
-    def generate_data_loop(self):
-
-        while self.is_data_generation_active:
-            tup = next(self.combinations_outstanding, None)
-
-            if tup is None:
-                self.is_data_generation_active = False
-
-            else:
-                self.combination_index, self.combination = tup[0], tup[1]
-                self.combination_string = "".join(letter for letter in self.combination)
-                self.generate_combination_data()
-
     def run(self):
 
         self.check_log()
-        self.generate_iterators()
+        self.print_progress()
+        self.generate_tasks()
 
         print("Generating new data...")
 
-        for method in [self.generate_data_loop, self.print_progress_loop, self.log_data_loop]:
+        for method in [self.generate_data_loop, self.process_data_loop, self.log_data_loop, self.print_progress_loop]:
             thread = threading.Thread(target = method, daemon = True)
             thread.start()
 
         #BLOCKING
         while self.is_active:
-            if (not self.is_data_generation_active) and (not self.is_data_logging_active):
+            if not self.is_logging_active:
                 self.is_active = False
             time.sleep(self.block_time)
 
         print("Data generation complete.")
 
+class GameDataGenerator(DataGenerator):
+
+    def __init__(self, output_file, profanity_input_file, solver):
+        super().__init__(output_file)
+
+        self.solver = solver
+        self.last_index = 657800 #26 choose 7
+        self.profanity_input_file = profanity_input_file
+        self.output_columns_list = ["COMBINATION_INDEX", "SEVEN_LETTERS", "CENTER_LETTER", "NUM_SOLUTIONS", "IS_PROFANE", "IS_PANGRAM"]
+        self.output_column_index = "COMBINATION_INDEX"
+
+        self.task_name = "combinations"
+
+    def generate_data(self, task_index): #INDEX > VALIDATOR TUPS
+        combinations = itertools.combinations(alphabet_string, 7)
+        combination = next(itertools.islice(combinations, task_index, task_index + 1))
+        combination_string = "".join(letter for letter in combination)
+
+        for center_letter in combination:
+            validator = Validator(self.solver, combination, center_letter, self.profanity_input_file)
+            validator_tup = (task_index, combination_string, center_letter, validator)
+            self.to_process.put(validator_tup)
+
+    def process_data(self, validator_tup): #VALIDATOR TUP > OUTPUTDICT
+
+        task_index = validator_tup[0]
+        combination_string, center_letter = validator_tup[1], validator_tup[2]
+        validator = validator_tup[3]
+        output_dict = {}
+
+        output_dict["COMBINATION_INDEX"] = task_index
+        output_dict["SEVEN_LETTERS"] = combination_string
+        output_dict["CENTER_LETTER"] = center_letter
+        output_dict["NUM_SOLUTIONS"] = validator.num_solutions()
+        output_dict["IS_PROFANE"] = validator.is_profane()
+        output_dict["IS_PANGRAM"] = validator.is_pangram()
+
+        self.to_log.put(output_dict)
+
 if __name__ == "__main__":
     solver = Solver("dictionary_data/wordswithfriends_dictionary.txt", 4)
-    generator = DataGenerator("solution_data.csv", "dictionary_data/profanity_dictionary.txt", solver)
-    generator.run()
+    game_generator = GameDataGenerator("solution_data.csv", "dictionary_data/profanity_dictionary.txt", solver)
+    game_generator.run()
